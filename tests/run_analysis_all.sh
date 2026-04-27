93b6ef98cb0d8151aa7e81df50cab0e297d15643
Nikasha Patel <nikashap@gmail.com>
2026-04-27 12:36:39 -0400

    Changed cutoff to April 14th, 2026 2:00 pm in bash script

--- tests/run_analysis_all.sh
+++ tests/run_analysis_all.sh
@@ -5,7 +5,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 DATA_DIR="$SCRIPT_DIR/../data"
-CUTOFF="26_04_14-12_01_00"
+CUTOFF="26_04_14-14_00_00"
 
 for dir in "$DATA_DIR"/*/; do
     date_name="$(basename "$dir")"
